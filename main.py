5cde6c9701bd9a5d06569be9db6712dbe6de23a8
Psykoxen <58462517+Psykoxen@users.noreply.github.com>
2021-12-24 19:58:18 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -20,29 +20,23 @@ certif = True
 
 ################################################################## - MAIN - ####################################################################
 while certif == True:
-	try:
-		r = requests.get('https://covid.ourworldindata.org/data/owid-covid-data.json').json()
-		data = r['FRA']['data'][-1]
+	data = requests.get('https://coronavirusapifr.herokuapp.com/data/live/france').json()[0]
+	print(data)
+	hst = open('date.json')
+	hst_date = json.load(hst)
+	hst.close()
 
-		hst = open('date.json')
-		hst_date = json.load(hst)
+	if len(hst_date['repertory'])==0 or (data['date'] != hst_date['repertory'][-1]):
+
+		hst = open('date.json','w')
+		hst_date['repertory'].append(data['date'])
+		json.dump(hst_date, hst)
 		hst.close()
 
-		if len(hst_date['repertory'])==0 or (data['date'] != hst_date['repertory'][-1]):
-
-			hst = open('date.json','w')
-			hst_date['repertory'].append(data['date'])
-			json.dump(hst_date, hst)
-			hst.close()
-
-			message = '|#COVID19| ~ '+datetime.datetime.now().strftime("{%w} %d {%m} 20%y").format(*days_FR,*mnths_FR)+' :\n\n---- ~ 😷 Contamination 😷 ~ ----\nNouveaux Cas : '+str(int(data['new_cases']))+'\nCas Totaux : '+str(int(data['total_cases']))+'\n--------- ~ ⚰️ Décès ⚰️ ~ ---------\nDécès du jour : '+str(int(data['new_deaths']))+'\nDécès Totaux : '+str(int(data['total_deaths']))+'\n------- ~ 🏥 Hôpitaux 🏥 ~ -------\nHospitalisations : '+str(int(data['hosp_patients']))+'\nSoins Intensif : '+str(int(data['icu_patients']))
-			api.update_status(message)
-			print(str(datetime.date.today())+' | TWEET | Covid Data')
-
-		else:
-			print(str(datetime.date.today())+' | TWEET | No Covid Data')
-
-		time.sleep(3600)
-
-	except:
-		print(str(datetime.date.today())+' | ERROR | Covid Data')
+		message = '|#COVID19| ~ '+datetime.date(int(data['date'][:4]),int(data['date'][5:7]),int(data['date'][8:])).strftime("{%w} %d {%m} 20%y").format(*days_FR,*mnths_FR)+' :\n\n---- ~ 😷 Contamination 😷 ~ ----\nNouveaux Cas : '+str(int(data['conf_j1']))+'\nCas Totaux : '+str(int(data['conf']))+'\n--------- ~ ⚰️ Décès ⚰️ ~ ---------\nDécès du jour : '+str(int(data['incid_dchosp']))+'\nDécès Totaux : '+str(int(data['dchosp']))+'\n------- ~ 🏥 Hôpitaux 🏥 ~ -------\nHospitalisations : '+str(int(data['hosp']))+'\nSoins Intensif : '+str(int(data['rea']))
+		#api.update_status(message)
+		print(message)
+		print(str(datetime.date.today())+' | TWEET | Covid Data')
+	else:
+		print(str(datetime.date.today())+' | TWEET | No Covid Data')
+	time.sleep(3600)
