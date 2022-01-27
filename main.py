973560f4d4039793ff191c8d95f4d3cec2ae02b4
Psykoxen <58462517+Psykoxen@users.noreply.github.com>
2022-01-27 11:36:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ api = tweepy.API(auth)
 
 ############################################################### - INITIALISATION - ####################################################################
 
-days_FR = ['Dimanche','Lundi','Mardi','Mercbright_redi','Jeudi','Vendbright_redi','Samedi']
+days_FR = ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi']
 mnths_FR = ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre']
 certif = True
 
@@ -25,7 +25,6 @@ while certif == True:
 		hst = open('date.json')
 		hst_date = json.load(hst)
 		hst.close()
-
 		if len(hst_date['repertory'])==0 or (data['date'] != hst_date['repertory'][-1]):
 
 			hst = open('date.json','w')
@@ -37,9 +36,9 @@ while certif == True:
 					data[i] = '/'
 			message = '|#COVID19| ~ '+datetime.date(int(data['date'][:4]),int(data['date'][5:7]),int(data['date'][8:])).strftime("{%w} %d {%m} 20%y").format(*days_FR,*mnths_FR)+' :\n\n---- ~ 😷 Contamination 😷 ~ ----\nNouveaux Cas : '+str(data['conf_j1'])+'\nCas Totaux : '+str(data['conf'])+'\n--------- ~ ⚰️ Décès ⚰️ ~ ---------\nDécès du jour : '+str(data['incid_dchosp'])+'\nDécès Totaux : '+str(data['dchosp'])+'\n------- ~ 🏥 Hôpitaux 🏥 ~ -------\nHospitalisations : '+str(data['hosp'])+'\nSoins Intensif : '+str(data['rea'])
 			api.update_status(message)
-			print('[bold green4]'+str(datetime.date.today())+' | TWEET | No Covid Data [/bold green4]')
+			print('[bold green4]'+str(datetime.date.today())+' | TWEET | Covid Data [/bold green4]')
 		else:
-			print('[bold dodger_blue2]'+str(datetime.date.today())+' | TWEET | Covid Data [/bold dodger_blue2]')
+			print('[bold dodger_blue2]'+str(datetime.date.today())+' | TWEET | No Covid Data [/bold dodger_blue2]')
 			time.sleep(3600)
 	except:
 		print('[bold bright_red]'+str(datetime.date.today())+' | ERROR | Covid API [/bold bright_red]')
